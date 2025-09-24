6b6276f9cc8af8e5dfa60968984309bce120d5ad
Code Nova <netdot1234@gmail.com>
2025-09-24 23:12:30 +0100

    env..

--- config/index.tsx
+++ config/index.tsx
@@ -2,13 +2,14 @@ import { cookieStorage, createStorage, noopStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId: string = process.env.NEXT_PUBLIC_PROJECT_ID ?? "";
+
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum]
+export const networks = [mainnet, arbitrum]; 
 
 // Always wrap in createStorage
 const storage = createStorage({
