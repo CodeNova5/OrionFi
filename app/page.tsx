ad3c37de6a285a710605f529110b758aa0acf90a
Code Nova <netdot1234@gmail.com>
2025-09-24 12:25:25 +0100

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
+
+import { AppKitButton } from "@reown/appkit/react";
+
 export default function HomePage() {
   return (
     <main>
       <h1>🚀 OrionFi</h1>
 
-      <appkit-button />
-
+      <AppKitButton />
     </main>
   );
 }
